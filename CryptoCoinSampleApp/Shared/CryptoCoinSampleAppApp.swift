4868033295fe6973b1af863d27b3ec9d4e36cada
Eric Vargas <eric@liblab.com>
2022-06-07 13:20:18 -0500

    Create sample app

---
+++ CryptoCoinSampleApp/Shared/CryptoCoinSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCoinSampleAppApp.swift
+//  Shared
+//
+//  Created by Eric Vargas on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCoinSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
