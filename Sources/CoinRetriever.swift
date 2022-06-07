7bfc097fd6cb606b499caab56ff41b277c45c438
Eric Vargas <eric@liblab.com>
2022-06-07 17:18:13 -0500

    Add sample endpoint test for cryptocurrency/listings/latest

--- Sources/CoinRetriever.swift
+++ Sources/CoinRetriever.swift
@@ -1,8 +1,20 @@
+import Alamofire
+
+
 public struct CoinRetriever {
+    private var apiKey: String;
     
     public init() {}
     
+    public func setApiKey(key: String) {
+        self.apiKey = key;
+    }
     public func latestPrice(coin: String) -> String {
+        let headers: HTTPHeaders = ["X-CMC_PRO_API_KEY": "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c"]
+//        https://pro-api.coinmarketcap.com
+        AF.request("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest", headers: headers).response { response in
+            debugPrint(response)
+        }
         return "10"
     }
 }
