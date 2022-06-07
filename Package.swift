3972f0ee85ac59073df6c41fe352eb842d821c6b
Eric Vargas <eric@liblab.com>
2022-06-07 13:23:51 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyCryptoCoinSwiftLibrary ",
+    name: "MyCryptoCoinSwiftLibrary",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyCryptoCoinSwiftLibrary ",
-            targets: ["MyCryptoCoinSwiftLibrary "]),
+            name: "MyCryptoCoinSwiftLibrary",
+            targets: ["MyCryptoCoinSwiftLibrary"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MyCryptoCoinSwiftLibrary ",
-            dependencies: []),
+            name: "MyCryptoCoinSwiftLibrary",
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "MyCryptoCoinSwiftLibrary Tests",
-            dependencies: ["MyCryptoCoinSwiftLibrary "]),
+            dependencies: ["MyCryptoCoinSwiftLibrary"]),
     ]
 )
