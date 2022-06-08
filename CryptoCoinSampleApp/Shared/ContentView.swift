a8317c1521dd57d4a38e75819248841d3e5838e4
Eric Vargas <eric@liblab.com>
2022-06-07 19:41:40 -0500

    Show coin prices as list in sample app

--- CryptoCoinSampleApp/Shared/ContentView.swift
+++ CryptoCoinSampleApp/Shared/ContentView.swift
@@ -1,21 +1,34 @@
-//
-//  ContentView.swift
-//  Shared
-//
-//  Created by Eric Vargas on 6/7/22.
-//
-
 import SwiftUI
+import MyCryptoCoinSwiftLibrary
 
 struct ContentView: View {
+    // prod
+//    let coinRetriever = CoinRetriever(apiKey: "0e3cd0cc-0e09-4693-9675-15a9d079ae68")
+    // sandbox
+    let coinRetriever = CoinRetriever(apiKey: "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c")
+    @State var coins = [Coin]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if coins.isEmpty {
+            Text("Loading").padding()
+        }
+        List(coins) { coin in
+            VStack(alignment: .center) {
+                Text("\(coin.name) price: \(coin.quote["USD"]!.price)")
+            }
+        }
+        .onAppear(perform: loadData)
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func loadData() {
+        let bitcoinSymbol = "BTC"
+        coinRetriever.latestPrice(coins: [bitcoinSymbol]) { result in
+            switch (result) {
+            case .success(let coins):
+                self.coins = coins.data[bitcoinSymbol]!
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
