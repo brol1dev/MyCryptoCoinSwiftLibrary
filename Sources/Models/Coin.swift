ae494eb75fa6b43aa2707af1c0670ae544e2957f
Eric Vargas <eric@liblab.com>
2022-06-07 19:05:21 -0500

    Make Coin identifiable

--- Sources/Models/Coin.swift
+++ Sources/Models/Coin.swift
@@ -1,5 +1,6 @@
 
-public struct Coin: Decodable {
+public struct Coin: Decodable, Identifiable {
+    public let id: Int
     public let name: String
     public let symbol: String
     public let quote: [String: Quote]
